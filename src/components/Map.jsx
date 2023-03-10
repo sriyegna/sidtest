4c9fc8670f2765cde41bb88eb6a4a5ee9306be2b
Sid Natarajan <sid.nat@gmail.com>
2023-03-10 18:20:56 -0500

    map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,52 +5,7 @@ import { MapContainer, ImageOverlay } from "react-leaflet";
 // import MapComponent from "./MapComponent";
 import { CRS } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-
-// <div style={{ width: "1100px", height: "600px" }}>
-//   <MapContainer
-//     style={{ height: "100%", minHeight: "100%" }}
-//     bounds={zoom ? undefined : bounds}
-//     boundsOptions={{
-//       padding: [0, 0]
-//     }}
-//     maxBounds={bounds}
-//     zoom={center ? zoom : undefined}
-//     center={zoom ? center : undefined}
-//     crs={CRS.Simple}
-//     zoomSnap={0} // Important to disable snap after fitBounds
-//     whenReady={(e) => e.target.fitBounds(bounds)} // Have the map adjust its view to the same bounds as the Image Overlay
-//   >
-//     <ImageOverlay
-//       url={skiMap}
-//       bounds={bounds}
-//       className="map_main"
-//     />
-//     <MapHandler />
-//   </MapContainer>
-// </div>
-
-// {/* <MapComponent
-// imageLink={skiMap}
-// // bounds={bounds}
-// // className="map_main"
-// /> */}
-
-// return (
-//   <>
-//     <MapContainer
-//       style={{ height: "100vh", width: "100vw" }}
-//       minZoom={0}
-//       crs={CRS.Simple}
-//       maxBoundsViscosity={1.0}
-//       boundsOptions={{ padding: [50, 50] }}
-
-//     >
-//       <MapComponent imageLink={skiMap} />
-//       <MapHandler />
-//     </MapContainer>
-//   </>
-// )
+// import L from "leaflet";
 
 const M = ({ width, height, zoom, center }) => {
   const wh = [width, 3250];
