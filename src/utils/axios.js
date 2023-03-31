0e59b0cdccaeae770bdea1b1f4099d5668d8a2ab
Sid Natarajan <sid.nat@gmail.com>
2023-03-30 23:14:36 -0400

    added token to axios instance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,13 @@
 import axios from "axios";
+import Cookies from 'universal-cookie'
+const cookies = new Cookies();
 
 const instance = axios.create({
   // baseURL: 'http://localhost:3003/'
-  baseURL: 'http://localhost:5000/'
+  baseURL: 'http://localhost:5000/',
+  headers: {
+    Authorization: `${cookies.get('token')}`
+  }
 });
 
 export default instance
